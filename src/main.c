94fabb4fb3d4d90800a191fb508149e20f4c3876
Erik Ekblad <oakl33f123@gmail.com>
2025-07-29 15:12:00 +0200

    modularized a little

--- src/main.c
+++ src/main.c
@@ -7,37 +7,49 @@
 #include <SDL3_ttf/SDL_ttf.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-
+typedef struct {
     SDL_Window *window;
-    bool done = false;
+    SDL_Renderer *renderer;
+    TTF_Font *font;
+} SDLContext;
 
-    SDL_Init(SDL_INIT_VIDEO);
-    TTF_Init();
+SDLContext initSDL() {
+    SDLContext ctx = {NULL};
 
-    // Create an application
-    window = SDL_CreateWindow("digit dive", 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    if (SDL_Init(SDL_INIT_VIDEO) == 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "SDL_Init failed %s", SDL_GetError());
+        return ctx;
+    }
 
-    if (window == NULL) {
+    if (TTF_Init() == 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "TTF_Init failed %s", SDL_GetError());
+        return ctx;
+    }
+
+    ctx.window = SDL_CreateWindow("digit dive", 640, 480, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+    if (!ctx.window) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
-        return 1;
+        return ctx;
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
-    if (!renderer) {
+    ctx.renderer = SDL_CreateRenderer(ctx.window, NULL);
+    if (!ctx.renderer) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create renderer: %s\n", SDL_GetError());
-        return 1;
+        return ctx;
     }
 
-    TTF_Font *font = TTF_OpenFont("assets/IBMPlexMono-Medium.ttf", 24);
-    if (!font) {
+    ctx.font = TTF_OpenFont("assets/IBMPlexMono-Medium.ttf", 24);
+    if (!ctx.font) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not load font: %s", SDL_GetError());
-        return 1;
+        return ctx;
     }
 
-    SDL_Color textColor = {0, 0, 0, 255};
+    return ctx;
+}
 
-    SDL_Surface *textSurface = TTF_RenderText_Blended(font, "Hello, World", 0, textColor);
+int renderTextLine(TTF_Font *font, SDL_Renderer *renderer, const char *text, float x, float y, SDL_Color textColor) {
+    SDL_Surface *textSurface = TTF_RenderText_Blended(font, text, 0, textColor);
     if (!textSurface) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not render texture: %s", SDL_GetError());
     }
@@ -52,28 +64,42 @@ int main(int argc, char *argv[]) {
     }
     SDL_DestroySurface(textSurface);
 
-    SDL_FRect textRect = {50.0f, 50.0f, (float)textW, (float)textH};
+    SDL_FRect textRect = {x, y, (float)textW, (float)textH};
 
-    while (!done) {
+    SDL_RenderTexture(renderer, textTexture, NULL, &textRect);
+    SDL_DestroyTexture(textTexture);
+
+    return 0;
+};
+
+int main(int argc, char *argv[]) {
+    bool run = true;
+
+    SDLContext ctx = initSDL();
+    if (!ctx.window || !ctx.renderer || !ctx.font) {
+        return 1;
+    }
+
+    SDL_Color textColor = {0, 0, 0, 255};
+
+    SDL_SetRenderDrawColor(ctx.renderer, 245, 255, 250, 255);
+    SDL_RenderClear(ctx.renderer);
+    renderTextLine(ctx.font, ctx.renderer, "hello world", 50.0, 50.0, textColor);
+    SDL_RenderPresent(ctx.renderer);
+
+    while (run) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT)
-                done = true;
+                run = false;
         }
-
-        SDL_SetRenderDrawColor(renderer, 245, 255, 250, 255);
-        SDL_RenderClear(renderer);
-
-        SDL_RenderTexture(renderer, textTexture, NULL, &textRect);
-
-        SDL_RenderPresent(renderer);
 
         SDL_Delay(16);
     }
 
-    TTF_CloseFont(font);
-    SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
+    TTF_CloseFont(ctx.font);
+    SDL_DestroyWindow(ctx.window);
+    SDL_DestroyRenderer(ctx.renderer);
     TTF_Quit();
     SDL_Quit();
     return 0;
